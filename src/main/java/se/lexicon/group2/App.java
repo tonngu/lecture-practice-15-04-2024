4cd28bd9754aba2fea1eba00ea532fbc9362ef94
Tony Nguyen <tonynguyen7396@gmail.com>
2024-04-15 10:36:32 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/group2/App.java
@@ -0,0 +1,15 @@
+package se.lexicon.group2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+}
---
+++ src/main/java/se/lexicon/group2/Product.java
@@ -0,0 +1,37 @@
+package se.lexicon.group2;
+
+public class Product {
+    private String productName;
+    private double price;
+    private int stock;
+
+    public Product(String productName, double price, int stock) {
+        this.productName = productName;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
